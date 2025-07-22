2097152c07fd79a1466a061615fa5f4cb4535a81
Shoozi <rudymayorga21@gmail.com>
2025-07-22 18:48:16 -0500

    added setwal script to zshrc for setting wallpapers and color schemes that exist outside of /themes

---
+++ bin/setWallpaper.zsh
@@ -0,0 +1,31 @@
+
+#!/bin/zsh
+
+if [[ -z "$1" ]]; then
+    echo "Usage: setWal path/to/image"
+    exit 1
+fi 
+
+if [[ ! -f "$1" ]]; then
+    echo "Error: there is no image at file path '$1'."
+    exit 1
+fi 
+
+WALLPAPER="$1"
+FASTFETCH_CONFIG="$HOME/.config/fastfetch/config.jsonc"
+MINARCH_PATH="$HOME/.config/fastfetch/ascii/minarch.txt"
+
+swww img "$WALLPAPER"
+wal -i "$WALLPAPER"
+
+# Set Fastfetch logo type to 'file' and source to minarch ascii file
+sed -i 's|"type": "builtin"|"type": "file"|' "$FASTFETCH_CONFIG"
+sed -i "s|\"source\": \".*\"|\"source\": \"$MINARCH_PATH\"|" "$FASTFETCH_CONFIG"
+
+pkill -USR swaync
+
+sleep 1
+
+clear
+fastfetch
+
